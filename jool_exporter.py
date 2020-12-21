467a76c8f8b18aacf8c6817c69c8b09c23879593
Cooper Lees <me@cooperlees.com>
2020-12-20 22:16:01 -0800

    Fix wrong license (MIT) to BSD in jool_exporter.py

--- jool_exporter.py
+++ jool_exporter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# This source code is licensed under the MIT license found in the
+# This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
 # coding=utf8
 
